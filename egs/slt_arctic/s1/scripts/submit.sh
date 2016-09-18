459cae2ba0eb97a401f459389ee33f07cd648789
filby <filntisis.panagiotis@gmail.com>
2016-09-19 01:21:31 +0300

    remove gpu_locking

--- egs/slt_arctic/s1/scripts/submit.sh
+++ egs/slt_arctic/s1/scripts/submit.sh
@@ -8,22 +8,8 @@ src_dir=$(dirname $1)
 # Source install-related environment variables
 source ${src_dir}/setup_env.sh
 
-# Try to lock a GPU...
-gpu_id=$(python ${src_dir}/gpu_lock.py --id-to-hog)
+echo "Running on GPU ..."
+THEANO_FLAGS="mode=FAST_RUN,device=gpu$gpu_id,"$MERLIN_THEANO_FLAGS
+export THEANO_FLAGS
 
-if [ $gpu_id -gt -1 ]; then
-    echo "Running on GPU id=$gpu_id ..."
-    THEANO_FLAGS="mode=FAST_RUN,device=gpu$gpu_id,"$MERLIN_THEANO_FLAGS
-    export THEANO_FLAGS
-    
-    python $@
-    
-    python ${src_dir}/gpu_lock.py --free $gpu_id
-else
-    echo "No GPU is available! Running on CPU..."
-
-    THEANO_FLAGS=$MERLIN_THEANO_FLAGS
-    export THEANO_FLAGS
-    
-    python $@
-fi
+python $@
